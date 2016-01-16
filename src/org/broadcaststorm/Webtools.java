a6d317f4cca6edc758b38d9ef6d3f6385ee378a1
needspeed <needspeed101@gmail.com>
2016-01-16 16:58:19 +0100

    Added tools for webstuff

---
+++ src/org/broadcaststorm/Webtools.java
@@ -0,0 +1,19 @@
+package org.broadcaststorm;
+
+import java.util.Map;
+
+/**
+ * Created by needspeed on 1/16/16.
+ */
+public class Webtools {
+    public static String UrlBuilder (String baseurl, Map<String, String> params) {
+        boolean first = true;
+        for (String k : params.keySet()) {
+            baseurl += (first) ? "?" : "&";
+            first = false;
+            baseurl += k + "=";
+            baseurl += params.get(k);
+        }
+        return baseurl;
+    }
+}
